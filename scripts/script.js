9f86779e2342769a2fb1cc3969eb4b6d3cdd692f
Jarrad <bluejay.jzp@gmail.com>
2023-08-14 09:52:32 +1000

    Removed Randomise order function test

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,21 @@
 let choiceType = "multiple";
 let difficulty = "easy";
 
-async function loadQuizQuestion() {
-    const question = await axios.get(`https://opentdb.com/api.php?amount=1&category=18&difficulty=${difficulty}&type=${choiceType}`);
-    const data = question.data[0];
+function loadQuizQuestion() {
+    axios.get(`https://opentdb.com/api.php?amount=1&category=18&difficulty=${difficulty}&type=${choiceType}`)
+        .then(response => {
+            const data = response.data.results[0];
+            console.log(data);
+            
+            const template = document.getElementById("template-quiz-card").content.cloneNode(true);
+            template.querySelector(".card-title").innerText = data["question"];
+            template.querySelector("#answer-a").innerText = data["incorrect_answers"][0];
+            template.querySelector("#answer-b").innerText = data["incorrect_answers"][1];
+            template.querySelector("#answer-c").innerText = data["correct_answer"];
+            template.querySelector("#answer-d").innerText = data["incorrect_answers"][2];
 
-    const template = document.getElementById("template-quiz-card").content.cloneNode(true);
-    template.querySelector(".card-title").innerText = data.question;
-    template.querySelector("#answer-a").innerText
-
-    document.querySelector("body").appendChild(template);
+            document.querySelector("body").appendChild(template);
+            
+        })
 }
 
-function RandomiseOrder(){
-    return Math.floor(Math.random());
-}
