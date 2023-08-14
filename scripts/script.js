c52958efa8c7a3f67f1cfbe464d9a592cb936a01
Jarrad <bluejay.jzp@gmail.com>
2023-08-14 20:08:28 +1000

    Added font styling for the quiz page

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,47 @@
 
+let previousScores = []; // Stores any atempt the player has made previously
 let difficulty = "easy"; // easy, medium , hard
-let numQuestions = 0;    // Number of questions that have been asked
+let numQuestions = 1;    // Number of questions that have been asked
+let difficultyInc = 7;   // Difficulty changes after every multiple of this
 let numCorrect = 0;      // Number of correct answers by the player
-let numAttempts = 1;     // The number of attempts the user is allowed to fail
+let numAttempts = 3;     // The number of attempts the user is allowed to fail
 let selected;            // The question selected by the player
-let correctAnswer;       // The 
+let correctAnswer;       // The correct answer to the current question
 
+/*
+  Sets the difficulty and fetches a question from the API. The data is loaded
+  into a template and displayed.
+*/
 function loadQuizQuestion() {
     // Reset the displaying container to remove the last question
     document.body.innerHTML = "";
 
-    if(numAttempts <= 0)
-        location.assign("../index.html")
+    // If the player has run out of attempts, reset the quiz
+    if(numAttempts <= 0) {
+        reset();
+        
+    }
+    
+    // Change the difficulty based on the number of questions answered
+    if(numQuestions > difficultyInc*2)
+        difficulty = "hard";
+    else if(numQuestions > difficultyInc)
+        difficulty = "medium";
+    else 
+        difficulty = "easy";
 
     // Fetch the next question based on the given difficulty
     axios.get(`https://opentdb.com/api.php?amount=1&category=18&difficulty=${difficulty}&type=multiple`)
         .then(response => {
             const data = response.data.results[0];
 
+            console.log(data);
+            console.log(`difficulty: ${difficulty}`);
+            console.log(`answer: ${data["correct_answer"]}`);
+            
             //This pools all options and randomly swaps their positions
             let options = [...data["incorrect_answers"], data["correct_answer"]];
-            swapRandomElements(options, 8);
+            swapRandomElements(options, 8); 
 
             const template = document.getElementById("template-quiz-card").content.cloneNode(true);
             template.querySelector("#attempts-count").innerHTML = "&#10060".repeat(numAttempts);
@@ -75,10 +96,19 @@ function onAnswerSubmitEvent(event) {
         }
     });
 
+    numQuestions ++;
     document.querySelector("#attempts-count").innerHTML = "&#10060".repeat(numAttempts);
     document.querySelector("#answer-count").innerText = numCorrect;
     document.querySelector("#button-submit").style.display = "none";
     document.querySelector("#button-next").style.display = "inline-block";
+}
+
+function reset() {
+    numQuestions = 1;
+    numCorrect = 0;
+    numAttempts = 3;
+    selected = null;
+    correctAnswer = "";
 }
 
 /*
