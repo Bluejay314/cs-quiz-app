74dfd581342bcdf203b59811f064ea19abbd6018
Jarrad <bluejay.jzp@gmail.com>
2023-08-14 12:47:04 +1000

    Answer option placement is now random

--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,19 @@ function loadQuizQuestion() {
     axios.get(`https://opentdb.com/api.php?amount=1&category=18&difficulty=${difficulty}&type=${choiceType}`)
         .then(response => {
             const data = response.data.results[0];
-            
+            let options = [...data["incorrect_answers"], data["correct_answer"]];
+            swapRandomElements(options, 4);
+            console.log(data["correct_answer"]);
+
             const template = document.getElementById("template-quiz-card").content.cloneNode(true);
             template.querySelector(".card-title").innerHTML = data["question"];
-            template.querySelector("#answer-a").innerHTML = data["incorrect_answers"][0];
-            template.querySelector("#answer-b").innerHTML = data["incorrect_answers"][1];
-            template.querySelector("#answer-c").innerHTML = data["correct_answer"];
-            template.querySelector("#answer-d").innerHTML = data["incorrect_answers"][2];
+            template.querySelector("#answer-a").innerHTML = options[0];
+            template.querySelector("#answer-b").innerHTML = options[1];
+            template.querySelector("#answer-c").innerHTML = options[2];
+            template.querySelector("#answer-d").innerHTML = options[3];
 
             correctAnswer = data["correct_answer"];
             document.querySelector("body").appendChild(template);
-            
         })
 }
 
@@ -34,5 +36,13 @@ function onAnswerSubmitEvent(event) {
         console.log("correct");
     else
         console.log("wrong");
+}
+
+function swapRandomElements(arr, numTimes) {
+    for(let i = 0; i < numTimes; i ++) {
+        let ix1 = Math.floor(Math.random() * arr.length);
+        let ix2 = Math.floor(Math.random() * arr.length);
+        [arr[ix1], arr[ix2]] = [arr[ix2], arr[ix1]];
+    }
 }
 
