db5a70d284a522361a74065c3a5663cd71e070f1
Jarrad <bluejay.jzp@gmail.com>
2023-08-14 11:27:03 +1000

    Uses innerHTML instead to allow special symbols

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,13 @@ function loadQuizQuestion() {
     axios.get(`https://opentdb.com/api.php?amount=1&category=18&difficulty=${difficulty}&type=${choiceType}`)
         .then(response => {
             const data = response.data.results[0];
-            console.log(data);
             
             const template = document.getElementById("template-quiz-card").content.cloneNode(true);
-            template.querySelector(".card-title").innerText = data["question"];
-            template.querySelector("#answer-a").innerText = data["incorrect_answers"][0];
-            template.querySelector("#answer-b").innerText = data["incorrect_answers"][1];
-            template.querySelector("#answer-c").innerText = data["correct_answer"];
-            template.querySelector("#answer-d").innerText = data["incorrect_answers"][2];
+            template.querySelector(".card-title").innerHTML = data["question"];
+            template.querySelector("#answer-a").innerHTML = data["incorrect_answers"][0];
+            template.querySelector("#answer-b").innerHTML = data["incorrect_answers"][1];
+            template.querySelector("#answer-c").innerHTML = data["correct_answer"];
+            template.querySelector("#answer-d").innerHTML = data["incorrect_answers"][2];
 
             document.querySelector("body").appendChild(template);
             
